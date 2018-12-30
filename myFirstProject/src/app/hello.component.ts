f588f822f6e16df0c18be6b60c4474bb6bd2f859
ananth516 <39372550+ananth516@users.noreply.github.com>
2018-12-30 22:50:18 +0530

    Add files via upload
    Initial Commit

---
+++ myFirstProject/src/app/hello.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class Hello {
+
+ @Input() uname:string;
+
+}
